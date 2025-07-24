07254fdae1316919584cd4fbbdfdbc144b89b7ac
Antonella Schillaci <antonella.schillaci145@gmail.com>
2025-07-24 10:52:19 +0200

    feat: create dynamic Recipe page with API fetch by ID

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import './index.scss'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home.tsx';
+import Recipe from './pages/Recipe/Recipe.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
+        <Route index element={<Home />} />
+        <Route path="/recipe/:id" element={<Recipe />} />
         </Route>
       </Routes>
     </BrowserRouter>
