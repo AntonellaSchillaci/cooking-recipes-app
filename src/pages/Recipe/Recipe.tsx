cacb104954e160be07e5700d5bb6a3a6d5a24375
Antonella Schillaci <antonella.schillaci145@gmail.com>
2025-07-24 11:20:27 +0200

    feat: add back button and ingredients list to Recipe page

--- src/pages/Recipe/Recipe.tsx
+++ src/pages/Recipe/Recipe.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Loader from "../../components/Loader/Loader";
 import "./styles.scss";
 
@@ -15,6 +15,9 @@ type Meal = {
 function Recipe() {
     const { id } = useParams();
     const [meal, setMeal] = useState<Meal | null>(null);
+    const navigate = useNavigate();
+    const ingredients: string[] = [];
+
   
     useEffect(() => {
       fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
@@ -26,13 +29,34 @@ function Recipe() {
     }, [id]);
   
     if (!meal) return <Loader />;
+
+    const mealData = meal as Record<string, string>;
+
+    for (let i = 1; i <= 20; i++) {
+        const ingredient = mealData[`strIngredient${i}`];
+        const measure = mealData[`strMeasure${i}`];
+        if (ingredient && ingredient.trim() !== '') {
+            ingredients.push(`${ingredient} - ${measure}`);
+        }
+    }
   
     return (
       <div className="recipe-detail">
+        <div className="back-container">
+            <button className="back-button" onClick={() => navigate('/')}>
+                ⬅️ Back to Recipes
+            </button>
+        </div>
         <h2 className="recipe-title">{meal.strMeal}</h2>
         <img className="recipe-img" src={meal.strMealThumb} alt={meal.strMeal} />
         <p className="recipe-sub-section"><strong className="recipes-strong">Categoria:</strong> {meal.strCategory}</p>
         <p className="recipe-sub-section"><strong className="recipes-strong">Cucina:</strong> {meal.strArea}</p>
+        <h3 className="recipe-title-instructions">Ingredienti</h3>
+        <ul className="list">
+        {ingredients.map((item, index) => (
+            <li className="list-element" key={index}>{item}</li>
+        ))}
+        </ul>
         <h3 className="recipe-title-instructions">Istruzioni</h3>
         <p className="recipe-sub-section">{meal.strInstructions}</p>
       </div>
