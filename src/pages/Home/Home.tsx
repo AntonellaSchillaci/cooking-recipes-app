752e586075455ba9c96803093a2823c82e3fd8b0
Antonella Schillaci <antonella.schillaci145@gmail.com>
2025-07-24 09:54:18 +0200

    feat: fetch recipes from API and display them with RecipeCard component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from 'react';
 import './styles.scss'; 
+import RecipeCard from '../../components/RecipeCard/RecipeCard';
+
+type Meal = {
+    idMeal: string;
+    strMeal: string;
+    strMealThumb: string;
+}
 
 function Home() {
+    const [meals, setMeals] = useState<Meal[]>([]);
+  
+    useEffect(() => {
+      fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+        .then((res) => res.json())
+        .then((data) => {
+          setMeals(data.meals);
+        })
+        .catch((err) => console.error('Errore nella fetch:', err));
+    }, []);
+    
     return (
-        <div className="title-container">
-            <h1 className="title">Benvenuta in Cooking Recipes App! 🍝</h1>
-            <p className="sub-title">Scopri piatti deliziosi e lasciati ispirare!</p>
+        <div className="home">
+            <div className="title-container">
+                <h1 className="title">Benvenuta in Cooking Recipes App! 🍝</h1>
+                <p className="sub-title">Scopri piatti deliziosi e lasciati ispirare!</p>
+            </div>
+            <div className="recipe-list">
+                {meals.map((meal) => (
+                    <RecipeCard 
+                        key={meal.idMeal}
+                        id={meal.idMeal}
+                        title={meal.strMeal}
+                        image={meal.strMealThumb}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
