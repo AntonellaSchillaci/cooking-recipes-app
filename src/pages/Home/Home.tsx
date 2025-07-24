4aac95c6c4eba8adf40c60a89af1e8f9a5369529
Antonella Schillaci <antonella.schillaci145@gmail.com>
2025-07-24 09:21:26 +0200

    feat: create Home page with initial layout and styles

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import './styles.scss'; 
+
+function Home() {
+    return (
+        <div className="title-container">
+            <h1 className="title">Benvenuta in Cooking Recipes App! 🍝</h1>
+            <p className="sub-title">Scopri piatti deliziosi e lasciati ispirare!</p>
+        </div>
+    )
+}
+
+export default Home;
